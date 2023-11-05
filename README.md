f8aa31116ac46bb70e5d77f5bf46342524ba70c7
kanhua <kanhua.lee@gmail.com>
2023-11-05 22:37:38 +0900

    Draw matches between the images

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-Playground of studing camera pose and pycolmap
+# Playground of studying 3D reconstruction, camera pose and camera calibration 
+
+## Install colmap and pycolmap
+
+
+
+## Calculate the relative pose between two images:
+
+- script: `get_rlative_pose.py`
+
+- This script uses the matched results by colmap and calculate the relative pose between two images.
+
+The dataset is downloaded from [colmap website](https://colmap.github.io/datasets.html). "South-Building" is used in this script.
+
+
+## Tips of using pycolmap
+
+```python
+
+
+for image_id, image in reconstruction.images.items():
+   print(image_id, image) 
+   print(image.points2D[0].point3D_id)  # get the point3D_id of the first point2D of the "image"
+   print(image.get_valid_point2D_ids()) # get the list of all the point2D ids whose point_3D_id is not -1
+   for valid_point_2D_id in image.get_valid_point2D_ids():
+       print(image.points2D[valid_point_2D_id].point3D_id)  # get the point3D_id of the valid point2D
+   print(image.ListPoint2D)
+
+ for point3D_id, point3D in reconstruction.points3D.items():
+    print(point3D_id, point3D)
+
+ for camera_id, camera in reconstruction.cameras.items():
+    print(camera_id, camera)
+
+```
--- get_relative_pose.py
+++ get_relative_pose.py
@@ -4,9 +4,19 @@ import tqdm
 from hloc.utils import viz
 import matplotlib.pyplot as plt
 import numpy as np
+import pathlib
+import cv2
+import logging
 
-
+logging.basicConfig(level=logging.INFO)
 def get_matches_from_query(reconstruction_model: Reconstruction, query_image_1_index: int, query_image_2_index: int):
+    """
+    Get matches from query images by going through all the feature points
+    :param reconstruction_model:
+    :param query_image_1_index:
+    :param query_image_2_index:
+    :return:
+    """
     query_image_1 = reconstruction_model.images[query_image_1_index]
     query_image_2 = reconstruction_model.images[query_image_2_index]
     image_1_coords = []
@@ -15,7 +25,7 @@ def get_matches_from_query(reconstruction_model: Reconstruction, query_image_1_i
         for valid_point_2D_id_2 in query_image_2.get_valid_point2D_ids():
             if query_image_1.points2D[valid_point_2D_id_1].point3D_id == query_image_2.points2D[
                 valid_point_2D_id_2].point3D_id:
-                print("found: {}".format(query_image_1.points2D[valid_point_2D_id_1].xy))
+                logging.info("found: {}".format(query_image_1.points2D[valid_point_2D_id_1].xy))
                 image_1_coords.append(query_image_1.points2D[valid_point_2D_id_1].xy)
                 image_2_coords.append(query_image_2.points2D[valid_point_2D_id_2].xy)
 
@@ -46,26 +56,28 @@ def get_matches_from_3d_points(reconstruction_model: Reconstruction, query_image
     return np.array(image_1_coords), np.array(image_2_coords)
 
 
-import pathlib
-import cv2
-
-
 def get_image_from_index(reconstruction_model: Reconstruction, image_index: int, image_dir):
     image_path = image_dir.joinpath(reconstruction_model.images[image_index].name)
     image = cv2.imread(str(image_path))
     return image
 
 
+def array_converter(arr):
+    list_of_arrays = [np.array([row[0], row[1]], dtype=np.float64) for row in arr]
+    return list_of_arrays
+
+
 def main():
     colmap_output = "datasets/south-building/sparse/"
     reconstruction = pycolmap.Reconstruction(colmap_output)
-    print(reconstruction.summary())
+    logging.info(reconstruction.summary())
 
+    # image 3 and image 4 have reasonable number of matches
     query_image_1_index = 3
     query_image_2_index = 4
     image_1_coords, image_2_coords = get_matches_from_3d_points(reconstruction, 3, 4)
-    print(image_1_coords)
-    print(image_2_coords)
+    logging.debug(image_1_coords)
+    logging.debug(image_2_coords)
 
     image_dir = pathlib.Path("datasets/south-building/images")
 
@@ -74,21 +86,28 @@ def main():
 
     viz.plot_images([image_1, image_2])
     viz.plot_matches(image_1_coords, image_2_coords)
-    plt.show()
+    plt.savefig("./outouts/matches_{:03d}_{:03d}.png".format(query_image_1_index, query_image_2_index))
 
-    # for image_id, image in reconstruction.images.items():
-    #    print(image_id, image)
-    #    print(image.points2D[0].point3D_id)
-    #    print(image.get_valid_point2D_ids())
-    #    for valid_point_2D_id in image.get_valid_point2D_ids():
-    #        print(image.points2D[valid_point_2D_id].point3D_id)
-    # print(image.ListPoint2D)
+    answer = pycolmap.fundamental_matrix_estimation(
+        image_1_coords,
+        image_2_coords
+        # [options],  # optional dict or pycolmap.RANSACOptions
+    )
+    logging.info("Fundamental matrix between these two images: {}".format(answer))
 
-    # for point3D_id, point3D in reconstruction.points3D.items():
-    #    print(point3D_id, point3D)
+    camera_model = reconstruction.cameras[1]
 
-    # for camera_id, camera in reconstruction.cameras.items():
-    #    print(camera_id, camera)
+    answer = pycolmap.essential_matrix_estimation(image_1_coords, image_2_coords, camera_model, camera_model)
+
+    tv_options = pycolmap.TwoViewGeometryOptions()
+    tv_options.compute_relative_pose = True
+
+    logging.info("calculate relateive pose:")
+    answer = pycolmap.estimate_calibrated_two_view_geometry_from_matches(camera_model, image_1_coords, camera_model,
+                                                                         image_2_coords, tv_options)
+
+    logging.info("relative pose: {}".format(answer.cam2_from_cam1))
+    logging.info("essential matrix: {}".format(answer.E))
 
     reconstruction.write(colmap_output)
 
